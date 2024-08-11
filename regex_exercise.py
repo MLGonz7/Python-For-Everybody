21dd26c6e573959a32c28fbceae4379b6f348907
MLGonz7 <102196599+MLGonz7@users.noreply.github.com>
2024-08-11 04:30:07 -0700

    Update regex_exercise.py

--- regex_exercise.py
+++ regex_exercise.py
@@ -1,3 +1,8 @@
+#In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the 
+#file and compute the sum of the numbers.
+
+
+
 import re
 
 handle = open('practice.txt', 'r')
